5a5546118bc2cb69b916f3b0e6d2f1d429a54b88
t9md <taqumd@gmail.com>
2019-02-01 15:58:02 +0900

    cleanup

--- filter/normalize_japanese.rb
+++ filter/normalize_japanese.rb
@@ -1,18 +1,17 @@
-PUNCTUATIONS = ".,、。"
-
 module Filter
+  PUNCTUATIONS = ".,、。"
+  NANI_NANI = %w(... … 〜 ~)
+  NANI_NANI_REGEX_PATTERN = NANI_NANI.map {|e| Regexp.escape(e)}.join('|')
+  
   def self.normalize_japanese(text)
     text = text.gsub(/^\[.+\]\s*/, "")
 
     # 遺棄物{いきぶつ}、遺棄船{いきせん} => いきぶつ、いきせん
     # ひどい,恐{おそ}ろしい => ひどい、おそろしい
-    regex_punctuations = /#{PUNCTUATIONS.split('').join('|')}/
-    text = text.gsub(/([^#{PUNCTUATIONS}]+?)\{(.+?)\}(#{PUNCTUATIONS})?(\s*)/, '\2\3\4')
+    text = text.gsub(/([^#{PUNCTUATIONS}]+?)\{(.+?)\}(#{PUNCTUATIONS.split('').join('|')})?(\s*)/, '\2\3\4')
     text = text.gsub(/(.*?)\{(.*?)\}(,|、)?(\s*)/, '\2\3\4')
-    text = text.gsub("...", "なになに")
-    text = text.gsub("〜", "なになに")
-    text = text.gsub("~", "なになに")
-    text = text.gsub("~", "なになに")
+    
+    text = text.gsub(/#{NANI_NANI_REGEX_PATTERN}/, "なになに")
     text
   end
 end
